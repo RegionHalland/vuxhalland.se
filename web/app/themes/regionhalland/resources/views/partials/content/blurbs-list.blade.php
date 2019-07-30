bcfea6e9a4661883dfd1437c97fb39609dd00a15
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-07-30 15:56:55 +0200

    Länktext på ikonlänkar genom aria labelledby

--- web/app/themes/regionhalland/resources/views/partials/content/blurbs-list.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/content/blurbs-list.blade.php
@@ -6,10 +6,12 @@
             <li class="left-align col col-12 sm-col-6 md-col-6 lg-col-4" >
                 <div class="rh-blurb mx1 my2" style="border-bottom: 4px solid #E99579; background-color: white;">
                     <div style="height: 200px; overflow:hidden;position:relative">
-                        <img src="{!! $blurbs['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
+                        <img src="{!! $blurbs['image_url'] !!}" alt="{!! $blurbs['image_alt'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                     </div>
                     <div class="mx2 pt2 rh-blurb__description" >
-                        <h3><a href="{{ $blurbs['link_url'] }}" target="{{ $blurbs['link_target'] }}"  class="rh-link--navigation"0>{{ $blurbs['link_title'] }}</a></h3>
+                        @if($blurbs['link_title'])
+                            <h3><a href="{{ $blurbs['link_url'] }}" target="{{ $blurbs['link_target'] }}"  class="rh-link--navigation">{{ $blurbs['link_title'] }}</a></h3>
+                        @endif
                         <p>{{ $blurbs['post_content'] }}</p>
                     </div>
                 </div>
--- web/app/themes/regionhalland/resources/views/partials/navigation/nav-site.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/navigation/nav-site.blade.php
@@ -1,30 +1,29 @@
-
-    <nav class="rh-header center" aria-label="Sidhuvud" style="max-width: 1440px; background: #F8F6F4; padding-top:0; padding-bottom: 0;">
-        <div class="rh-block-on-mobile rh-responsive-headline px3 rh-visual-element--vuxhalland-logo-stripe" >
-            <strong>
-                <a style="text-decoration:none; color:black;" href="/">
-                    Yrkesutbildning i Halland
+@php( $id = uniqid())
+<nav class="rh-header center" aria-label="Sidhuvud" style="max-width: 1440px; background: #F8F6F4; padding-top:0; padding-bottom: 0;">
+    <div class="rh-block-on-mobile rh-responsive-headline px3 rh-visual-element--vuxhalland-logo-stripe" >
+        <strong>
+            <a style="text-decoration:none; color:black;" href="/">
+                Yrkesutbildning i Halland
+            </a>
+        </strong>
+    </div>
+    <div class="hidden-sm rh-visual-element--triangle"></div>
+    {{-- Utilities --}}
+    <div class="rh-toplinks" style="background: #F8F6F4;">
+        <ul class="rh-toplinks-nav" aria-label="Genvägar i sidhuvudet">
+            <li class="rh-block-on-mobile" style="line-height:2">
+                <a onclick="toggleBar();" aria-labelledby="bapluslogo" href="#" class="icon-volume-1 rh-round-button rh-round-button--vuxhalland"></a>
+                <a id="bapluslogo" class="rh-toplinks-nav-link logo" title="Aktivera Talande Webb" onclick="toggleBar();" href="#">
+                    Talande Webb
                 </a>
-            </strong>
-        </div>
-        <div class="hidden-sm rh-visual-element--triangle"></div>
-        {{-- Utilities --}}
-        <div class="rh-toplinks" style="background: #F8F6F4;">
-            <ul class="rh-toplinks-nav" aria-label="Genvägar i sidhuvudet">
-                <li class="rh-block-on-mobile" style="line-height:2">
-                    <a onclick="toggleBar();" href="#" class="icon-volume-1 rh-round-button rh-round-button--vuxhalland"></a>
-                    <a id="bapluslogo" class="rh-toplinks-nav-link logo" title="Aktivera Talande Webb" onclick="toggleBar();" href="#">
-                        Talande Webb
-                    </a>
-                </li>
-                <li class="rh-block-on-mobile" style="line-height:2">
-                    <a href="./om-oss" class="icon-arrow-right rh-round-button rh-round-button--vuxhalland"></a>
-                    <a class="rh-toplinks-nav-link logo pr3" href="./om-oss">
-                        Om oss
-                    </a>
-                </li>
-            </ul>
-        </div>
-
-        {{-- Utilities END --}}
-    </nav>
+            </li>
+            <li class="rh-block-on-mobile" style="line-height:2">
+                <a href="./om-oss" aria-labelledby="{{$id}}" class="icon-arrow-right rh-round-button rh-round-button--vuxhalland"></a>
+                <a id="{{$id}}" class="rh-toplinks-nav-link logo pr3" href="./om-oss">
+                    Om oss
+                </a>
+            </li>
+        </ul>
+    </div>
+    {{-- Utilities END --}}
+</nav>
